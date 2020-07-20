6a13dcb30e674a7a419df1172b5b6bb4eb42842f
MartinVald2170 <noways2170@gmail.com>
2020-07-20 11:38:30 +1000

    working on routes

--- budgetapp_frontend/src/index.js
+++ budgetapp_frontend/src/index.js
@@ -1,17 +1,31 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import React from "react"
+import ReactDOM from "react-dom"
+import Home from "./pages/Home";
+import FAQ from "./pages/FAQ";
+import Settings from "./pages/Settings";
+import Crud from "./pages/CRUD";
+import Budget from "./pages/Budget";
+import {BrowserRouter, Route} from "react-router-dom";
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+class App extends Component {
+
+
+    render() {
+        return (
+            <div>
+                <BrowserRouter>
+                    <Route exact path="/" component={Home} />
+                    <Route exact path="/FAQ" component={FAQ} />
+                    <Route exact path="/settings" component={Settings} />
+                    <Route exact path="/form" component={Crud} />
+                    <Route exact path="/budget" component={Budget} />
+                
+                </BrowserRouter>
+            </div>
+        );
+    };
+};
+
+
+export default App; 
